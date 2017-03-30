8febd02e51d0c3fbc35e575ee2bedda01b92475c
thomas kendall <thomas kendall>
2017-03-30 18:03:40 -0400

    Did variable work on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  {{name}}
+  <h1>{{score}} Beat the other team by {{score - lscore}}</h1>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+  score= 25;
+  lscore= 5;
+
+ }
